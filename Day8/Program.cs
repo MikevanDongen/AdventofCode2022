bc0e5c472f931a53c3fc52d40d2bb3c19856384c
Mike van Dongen <mike@holymonkey.nl>
2022-12-09 21:33:54 +0100

    Day8 & Day9

---
+++ Day8/Program.cs
@@ -0,0 +1,116 @@
+﻿using var fileStream = File.OpenRead(@"C:\Repos\AdventofCode2022\Day8\input.txt");
+using var streamReader = new StreamReader(fileStream);
+
+var map = new List<int[]>();
+do
+{
+    var line = await streamReader.ReadLineAsync();
+    if (line == null) break;
+
+    map.Add(line.Select(c => c - '0').ToArray());
+} while (true);
+
+Console.WriteLine("Part 1");
+{
+    var visibleTrees = new HashSet<string>();
+
+    LookAtTreelineX(true);
+    LookAtTreelineX(false);
+    LookAtTreelineY(true);
+    LookAtTreelineY(false);
+
+    Console.WriteLine(visibleTrees.Distinct().Count());
+
+    void LookAtTreelineX(bool directionAscending)
+    {
+        for (int y = 0; y < map.Count; y++)
+        {
+            var highestTree = -1;
+            for (int x = directionAscending ? 0 : map[y].Length - 1; x < map[y].Length && x > -1; x += directionAscending ? 1 : -1)
+            {
+                if (map[y][x] > highestTree)
+                {
+                    visibleTrees!.Add($"{y},{x}");
+                    highestTree = map[y][x];
+                }
+            }
+        }
+    }
+
+    void LookAtTreelineY(bool directionAscending)
+    {
+        for (int x = 0; x < map[0].Length; x++)
+        {
+            var highestTree = -1;
+            for (int y = directionAscending ? 0 : map.Count - 1; y < map.Count && y > -1; y += directionAscending ? 1 : -1)
+            {
+                if (map[y][x] > highestTree)
+                {
+                    visibleTrees!.Add($"{y},{x}");
+                    highestTree = map[y][x];
+                }
+            }
+        }
+    }
+}
+
+Console.WriteLine();
+Console.WriteLine("Part 2");
+{
+    var treeValues = new Dictionary<string, int>();
+
+    LookAtTreelineX(true);
+    LookAtTreelineX(false);
+    LookAtTreelineY(true);
+    LookAtTreelineY(false);
+
+    Console.WriteLine(treeValues.Values.Max());
+
+    void LookAtTreelineX(bool directionAscending)
+    {
+        for (int y = 0; y < map.Count; y++)
+        {
+            var visibleTrees = InitializeVisibleTrees();
+            for (int x = directionAscending ? 0 : map[y].Length - 1; x < map[y].Length && x > -1; x += directionAscending ? 1 : -1)
+            {
+                ProcessTree(y, x, ref visibleTrees);
+            }
+        }
+    }
+
+    void LookAtTreelineY(bool directionAscending)
+    {
+        for (int x = 0; x < map[0].Length; x++)
+        {
+            var visibleTrees = InitializeVisibleTrees();
+            for (int y = directionAscending ? 0 : map.Count - 1; y < map.Count && y > -1; y += directionAscending ? 1 : -1)
+            {
+                ProcessTree(y, x, ref visibleTrees);
+            }
+        }
+    }
+
+    int[] InitializeVisibleTrees()
+    {
+        return Enumerable.Repeat(0, 10).ToArray();
+    }
+
+    void ProcessTree(int y, int x, ref int[] visibleTrees)
+    {
+        var treeSize = map[y][x];
+        int value = visibleTrees[treeSize];
+
+        var key = $"{y},{x}";
+        if (!treeValues.TryAdd(key, value))
+        {
+            treeValues[key] *= value;
+        }
+
+        for (int i = 0; i < visibleTrees.Length; i++)
+        {
+            visibleTrees[i] = 1 + (i <= treeSize ? 0 : visibleTrees[i]);
+        }
+    }
+}
+
+Console.WriteLine();
---
+++ Day9/Program.cs
@@ -0,0 +1,86 @@
+﻿Console.WriteLine("Part 1");
+{
+    using var fileStream = File.OpenRead(@"C:\Repos\AdventofCode2022\Day9\input.txt");
+    using var streamReader = new StreamReader(fileStream);
+
+    var tailPosition = (X: 0, Y: 0);
+    var headPosition = (X: 0, Y: 0);
+    var tailPositions = new HashSet<string>() { "0,0" };
+    do
+    {
+        var line = await streamReader.ReadLineAsync();
+        if (line == null) break;
+
+        var newRelativeHeadPosition = GetNewRelativeHeadPosition(line);
+        for (int i = int.Parse(line[2..]); i > 0; i--)
+        {
+            headPosition = MovePosition(headPosition, newRelativeHeadPosition);
+            tailPosition = CalculateNewTailPosition(tailPosition, headPosition);
+            tailPositions.Add($"{tailPosition.X},{tailPosition.Y}");
+        }
+    } while (true);
+
+    Console.WriteLine(tailPositions.Count);
+}
+
+Console.WriteLine();
+Console.WriteLine("Part 2");
+{
+    using var fileStream = File.OpenRead(@"C:\Repos\AdventofCode2022\Day9\input.txt");
+    using var streamReader = new StreamReader(fileStream);
+
+    var knotPositions = Enumerable.Repeat((X: 0, Y: 0), 10).ToArray();
+    var tailPositions = new HashSet<string>() { "0,0" };
+    do
+    {
+        var line = await streamReader.ReadLineAsync();
+        if (line == null) break;
+
+        var newRelativeHeadPosition = GetNewRelativeHeadPosition(line);
+        for (int i = int.Parse(line[2..]); i > 0; i--)
+        {
+            knotPositions[0] = MovePosition(knotPositions[0], newRelativeHeadPosition);
+            for (int k = 1; k < knotPositions.Length; k++)
+            {
+                knotPositions[k] = CalculateNewTailPosition(knotPositions[k], knotPositions[k - 1]);
+            }
+
+            tailPositions.Add($"{knotPositions[9].X},{knotPositions[9].Y}");
+        }
+    } while (true);
+
+    Console.WriteLine(tailPositions.Count);
+}
+
+(int X, int Y) GetNewRelativeHeadPosition(string line)
+{
+    return line[0] switch
+    {
+        'U' => (0, 1),
+        'D' => (0, -1),
+        'L' => (-1, 0),
+        'R' => (1, 0),
+        _ => throw new NotImplementedException(),
+    };
+}
+
+(int X, int Y) MovePosition((int X, int Y) absolute, (int X, int Y) relative)
+{
+    return (absolute.X + relative.X, absolute.Y + relative.Y);
+}
+
+(int X, int Y) CalculateNewTailPosition((int X, int Y) tailPosition, (int X, int Y) headPosition)
+{
+    var headPositionRelativeToTail = (X: headPosition.X - tailPosition.X, Y: headPosition.Y - tailPosition.Y);
+    return headPositionRelativeToTail switch
+    {
+        (-1 or 0 or 1, -1 or 0 or 1) => tailPosition,
+        (int x, int y) => (tailPosition.X + Normalize(x), tailPosition.Y + Normalize(y)),
+    };
+}
+
+int Normalize(int z)
+{
+    if (z == 0) return 0;
+    return z > 0 ? 1 : -1;
+}
